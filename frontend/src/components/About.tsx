7405182a36f64dfb1f7f60f2f9a6838e323269cc
Manjula Prashan <87034519+Redsniper99@users.noreply.github.com>
2024-10-22 01:59:49 +0530

    hero section updated

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -1,17 +1,17 @@
-import mainMenu from "../assets/mainMenu.png";
-import feature1 from "../assets/f1.png";
-import feature2 from "../assets/f2.png";
-import feature3 from "../assets/f3.png";
+import mainMenu from "../assets/5.png";
+import feature1 from "../assets/1.png";
+import feature2 from "../assets/4.png";
+import feature3 from "../assets/5.png";
 
 export const About = () => {
   return (
     <section id="about" className="container py-24 sm:py-32">
       <div className="bg-muted/50 border rounded-lg py-12">
-        <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
+        <div className="px-5 flex flex-col-reverse md:flex-row gap-8 md:gap-1">
           <img
             src={mainMenu}
             alt="BlindSafe App"
-            className="w-[300px] object-contain rounded-lg"
+            className="w-[500px] object-contain rounded-lg"
           />
           <div className="bg-green-0 flex flex-col justify-between">
             <div className="pb-6">
@@ -29,11 +29,11 @@ export const About = () => {
                 ensuring that users can confidently navigate through daily
                 challenges without external assistance.
               </p>
-              {/* Increased size of the images */}
-              <div className="flex justify-center gap-4 mt-12">
-                <img src={feature1} alt="Feature 1" className="w-96 h-96 object-contain" />
-                <img src={feature2} alt="Feature 2" className="w-96 h-96 object-contain" />
-                <img src={feature3} alt="Feature 3" className="w-96 h-96 object-contain" />
+              {/* Reduced gap between the images */}
+              <div className="flex justify-center mt-12">
+                <img src={feature1} alt="Feature 1" className="w-80 h-80 object-contain " />
+                <img src={feature2} alt="Feature 2" className="w-80 h-80 object-contain " />
+                <img src={feature3} alt="Feature 3" className="w-80 h-80 object-contain " />
               </div>
             </div>
           </div>
